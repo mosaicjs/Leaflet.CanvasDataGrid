d79a215ae7a9862b49ec83c64d84376c48453cd2
mkotelnikov <mikhail.kotelnikov@ubimix.com>
2016-01-06 09:06:11 +0100

    Update package name

---
+++ test/index.js
@@ -0,0 +1,6 @@
+require("babel-core/register")({
+    // This will override `node_modules` ignoring - you can alternatively pass
+    // an array of strings to be explicitly matched or a regex / glob
+    ignore : false
+});
+// require('./XxxTest');
